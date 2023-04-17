f33ef828d24bf635d03a87e4e4d6586e298c5aaa
denizdagli <denizdaglice@gmail.com>
2023-04-17 23:44:41 +0300

    Template engine entegrasyonu eklendi

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = 5000;
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port} port`);
+  console.log(`Server is running on port ${port} `);
 });
